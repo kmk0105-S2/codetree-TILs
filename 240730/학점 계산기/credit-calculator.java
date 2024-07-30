5756ed68533aa12ffa31a331db4120437e688ebb
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2024-07-30 18:58:34 +0900

    Title: 학점 계산기, Time: 149ms, Memory: 12MB, Status: Passed - CodeTree

--- 240730/학점 계산기/credit-calculator.java
+++ 240730/학점 계산기/credit-calculator.java
@@ -5,16 +5,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int count = scanner.nextInt();
+        double sum = 0;
+
         double [] grd = new double [count];
 
-        for(int i = 0; i<count i++){
+        for(int i = 0; i<count; i++){
             grd[i] = scanner.nextDouble();
-            double sum += grd[i];
+            sum += grd[i];
         }
 
         double avg = sum / count;
 
-        System.out.print("%.lf\n", avg);
+        System.out.printf("%.1f\n", avg);
 
         if(avg >= 4.0){
             System.out.println("Perfect");
