4235689a1b8ab1782359e2da911a8deb180a4eae
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2024-07-30 18:26:52 +0900

    Title: 배열 채우고 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        
+        char [] ch = new char[10]; 
+
+        for(int i = 0; i<ch.length; i++){
+            ch[i] = scanner.next().charAt(0);
+        }
+
+        for(int i = ch.length; i>=0; i--){
+            System.out.print(ch[i]+" ");
+        }
+
+    }
+}
