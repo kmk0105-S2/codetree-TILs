913115fca70454c21763ba4e96019ab1a3d5472f
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2024-08-04 23:04:07 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 424ms, Memory: 15MB, Status: Passed - CodeTree

--- 240804/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 240804/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -1,48 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        int [] val = new int [n];
+        List<Integer> val = new ArrayList<>();
 
-        for(int i = 0; i < n; i++){
-            val[i] = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            val.add(scanner.nextInt());
         }
 
-        int max = 0;
-        int cnt = 0;
+        while (!val.isEmpty()) {
+            int max = Integer.MIN_VALUE;
+            int index = -1;
 
-        for(int j = 0; j < n; j++){
-            if(val[j] > max){
-                max = val[j];
-                cnt = j+1;
-            }
-        }
-
-        System.out.printf("%d ", cnt);
-
-        int max2 = 0;
-        int cnt2 = 0;
-        
-        while(true){
-            for(int k = 0; k < cnt-1; k++){
-                if(val[k] > max2){
-                    max2 = val[k];
-                    cnt2 = k+1;
+            for (int i = 0; i < val.size(); i++) {
+                if (val.get(i) > max) {
+                    max = val.get(i);
+                    index = i;
                 }
             }
 
-            if(cnt2 != 1){
-                System.out.printf("%d ", cnt2);
-                cnt = cnt2;
-                max2 = 0;
-            }
-            else{
-                System.out.printf("%d", cnt2);
-                break;
-            }
+            System.out.printf("%d ", index + 1);
+            val = val.subList(0, index);
         }
     }
 }
