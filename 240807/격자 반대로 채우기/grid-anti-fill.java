5961e230e21d3db86d585a5f09c3442aa1e6ed56
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2024-08-07 18:03:44 +0900

    Title: 격자 반대로 채우기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240807/격자 반대로 채우기/grid-anti-fill.java
+++ 240807/격자 반대로 채우기/grid-anti-fill.java
@@ -5,43 +5,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        int [][] arr = new int [n][n];
+        int [][] arr = new int[n][n];
         int cnt = 1;
-        int cnt2 = 6;
 
-        for(int col = n-1; col >=0; col-=2){
-            int row = n-1;
-
-            while(true){
-                arr[row][col] = cnt;
-                row--;
-                cnt++;
-
-                if(row<0){
-                    cnt += n;
-                    break;
+        for (int col = n - 1; col >= 0; col--) {
+            if ((n - col) % 2 == 1) {
+                // 아래에서 위로 채우기
+                for (int row = n - 1; row >= 0; row--) {
+                    arr[row][col] = cnt++;
+                }
+            } else {
+                // 위에서 아래로 채우기
+                for (int row = 0; row < n; row++) {
+                    arr[row][col] = cnt++;
                 }
             }
         }
 
-        for(int col = n-2; col >=0; col-=2){
-            int row = 0;
-            
-            while(true){
-                arr[row][col] = cnt2;
-                row++;
-                cnt2++;
-
-                if(row==n){
-                    cnt2 += n;
-                    break;
-                }
-            }
-        }
-        
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.printf("%d ", arr[i][j]);
             }
             System.out.println();
