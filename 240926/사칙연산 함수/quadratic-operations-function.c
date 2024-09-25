07f1accc641b19eaa142e2876d96534d892d8737
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2024-09-26 00:50:29 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240926/사칙연산 함수/quadratic-operations-function.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int plus(int a, int c){
+    return a+c;
+}
+
+int minus(int a, int c){
+    if(a >= c){
+        return a-c;
+    }
+    else{
+        return c-a;
+    }
+}
+
+int rhq(int a, int c){
+    return a*c;
+}
+
+int sksnrl(int a, int c){
+    if(a >= c){
+        return a/c;
+    }
+    else{
+        return c/a;
+    }
+}
+
+
+
+int main() {
+    int a, c;
+    char o;
+    scanf("%d %c %d", &a, &o, &c);
+
+    switch(o){
+        case '+':
+        printf("%d %c %d = %d", a, o, c, plus(a,c));
+        break;
+
+        case '-':
+        printf("%d %c %d = %d", a, o, c, minus(a,c));
+        break;
+
+        case '*':
+        printf("%d %c %d = %d", a, o, c, rhq(a,c));
+        break;
+
+        case '/':
+        printf("%d %c %d = %d", a, o, c, sksnrl(a,c));
+        break;
+
+        default:
+        printf("False");
+        break;
+    }
+
+    return 0;
+}
